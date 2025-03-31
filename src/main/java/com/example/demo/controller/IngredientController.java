d760fb3cd384a29e2422fd664e15f4e4e8b8c420
Nomena Fitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-03-31 17:34:40 +0300

    feat (IngredientController): add message for exception

--- src/main/java/com/example/demo/controller/IngredientController.java
+++ src/main/java/com/example/demo/controller/IngredientController.java
@@ -9,10 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -20,18 +18,21 @@ public class IngredientController {
     private Ingredient i1;
     private Ingredient i2;
 
-{   i1 = new Ingredient();
-    i1.setId(1L);
-    i1.setName("Oeuf");
-    i1.setUnitPrice(1000.0);
-    i1.setUpdatedAt(LocalDateTime.parse("2025-03-01T00:00:00"));
-
-    i2 = new Ingredient();
-    i2.setId(2L);
-    i2.setName("Huile");
-    i2.setUnitPrice(10000.0);
-    i2.setUpdatedAt(LocalDateTime.parse("2025-03-20T00:00:00"));
-}
+        {   i1 = new Ingredient();
+            i1.setId(1L);
+            i1.setName("Oeuf");
+            i1.setUnitPrice(1000.0);
+            i1.setUpdatedAt(LocalDateTime.parse("2025-03-01T00:00:00"));
+        
+            i2 = new Ingredient();
+            i2.setId(2L);
+            i2.setName("Huile");
+            i2.setUnitPrice(10000.0);
+            i2.setUpdatedAt(LocalDateTime.parse("2025-03-20T00:00:00"));
+        }
+        
+    
+    
 
     public List<Ingredient> getAllIngredientsWithMaxPrice(double priceMaxFilter, List<Ingredient> list){
 
@@ -51,12 +52,15 @@ public class IngredientController {
     }
 
     @GetMapping("/ingredients")
-    public ResponseEntity<Object> getAll(@RequestParam(name = "priceMinFilter", required = false) Double priceMinFilter, @RequestParam(name = "priceMaxFilter", required = false) Double priceMaxFilter) {
+    public ResponseEntity<Object> getAll(@RequestParam(name = "priceMinFilter", required = false) Double priceMinFilter, @RequestParam(name = "priceMaxFilter", required = false) Double priceMaxFilter) {      
+
         List<Ingredient> iList = List.of(i1, i2);
 
         if (priceMaxFilter != null && priceMaxFilter < 0 || priceMinFilter != null && priceMinFilter < 0 || priceMaxFilter != null && priceMinFilter != null && priceMaxFilter < priceMinFilter) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+            String responseBody = "Filters can't be a negative value  and priceMaxFilter: " + priceMaxFilter + " can't be smaller than priceMinFilter: " + priceMinFilter;
+            return new ResponseEntity<>( responseBody, HttpStatus.BAD_REQUEST);
+        } 
+
 
         List<Ingredient> response = new ArrayList<>();
 
@@ -73,7 +77,10 @@ public class IngredientController {
       
         
 
+        System.out.println("priceMinFilter: " + priceMinFilter);
+        System.out.println("priceMaxFilter: " + priceMaxFilter);
         return ResponseEntity.ok(response);
+        
 
     }
     
